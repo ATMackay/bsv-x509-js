9e35d3eb3d27afed595e900df4b6dacfb83a7b13
Alexander Mackay <a.mackay@nchain.com>
2020-02-18 13:53:57 +0000

    Initial commit.

---
+++ main.js
@@ -0,0 +1,31 @@
+// Requires Bitcoin SV library 
+const bsv = require("bsv")
+
+
+
+
+
+
+function create_certificate(){
+    console.log("Creating certificate")
+
+    }
+
+function verify_certificate(){
+    console.log("Validating certificate")
+
+    }
+
+function main(){
+    
+    console.log("Enter ..")
+    var choice = prompt("Enter number:")
+
+    if (choice != 1 || choice != 2){
+        console.log("Exiting...")
+        exit()
+    }
+
+    }
+
+
